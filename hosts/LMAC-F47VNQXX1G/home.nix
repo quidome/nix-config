75f6880d5b50d609db7b029cdab86886297426b1
Quido Meijer <quidome@gmail.com>
2023-02-27 18:21:42 +0100

    Workaround to that apps show up in spotlight
    Spotlight and Alfred don't follow symlinks to other devices (/nix/store
    for instance) so until that is fixed we'll just copy the apps to a
      "managed" directory. A bit dirty but it works.

--- hosts/LMAC-F47VNQXX1G/home.nix
+++ hosts/LMAC-F47VNQXX1G/home.nix
@@ -47,4 +47,30 @@
     ];
 
   # Misc configuration files --------------------------------------------------------------------{{{
+
+
+  # https://github.com/nix-community/home-manager/issues/1341#issuecomment-778820334
+  # This piece of code copies all the installed app to /Applications so that Alfred picks them up
+  home.activation = {
+    copyApplications =
+      let
+        apps = pkgs.buildEnv {
+          name = "home-manager-applications";
+          paths = config.home.packages;
+          pathsToLink = "/Applications";
+        };
+      in
+      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        baseDir="$HOME/Applications/Home Manager Apps.localized"
+        if [ -d "$baseDir" ]; then
+          rm -rf "$baseDir"
+        fi
+        mkdir -p "$baseDir"
+        for appFile in ${apps}/Applications/*; do
+          target="$baseDir/$(basename "$appFile")"
+          $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
+          $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
+        done
+      '';
+  };
 }
