a9008a0049c9a4cadcf92a31e4007b746e4918a7
Quido Meijer <quidome@gmail.com>
2023-03-01 20:37:22 +0100

    Remove unused configuration

--- hosts/LMAC-F47VNQXX1G/configuration.nix
+++ hosts/LMAC-F47VNQXX1G/configuration.nix
@@ -22,19 +22,11 @@
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
 
-  # Apps
-  # `home-manager` currently has issues adding them to `~/Applications`
-  # Issue: https://github.com/nix-community/home-manager/issues/1341
-  # environment.systemPackages = with pkgs; [ kitty terminal-notifier ];
-
   programs.nix-index.enable = true;
 
   # Fonts
   fonts.fontDir.enable = true;
   fonts.fonts = with pkgs; [
-    font-awesome
-    noto-fonts
-    noto-fonts-emoji
     source-code-pro
     recursive
     (nerdfonts.override {
