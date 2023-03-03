bbf174bfccfb1511df6985e6833f06ef377b61a8
Quido Meijer <quidome@gmail.com>
2023-03-03 16:41:00 +0100

    Nixify alacritty configuration

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -7,14 +7,56 @@ in
     enable = lib.mkEnableOption "Enable alacritty";
   };
 
-  config = lib.mkIf cfg.enable {
-    programs.alacritty.enable = true;
+  config = lib.mkIf cfg.enable
+    {
+      programs.alacritty.enable = true;
 
-    # manage config files
-    xdg.configFile = {
-      "alacritty/alacritty.yml".source = ./alacritty/alacritty.yml;
-      "alacritty/monokai.yaml".source = ./alacritty/monokai.yaml;
-      "alacritty/tender.yaml".source = ./alacritty/tender.yaml;
+      programs.alacritty.settings = {
+        selection.save_to_clipboard = true;
+        cursor.style = "beam";
+
+        font = {
+          size = 14;
+
+          normal.family = "JetBrainsMono Nerd Font";
+          normal.style = "Regular";
+          bold.family = "JetBrainsMono Nerd Font";
+          bold.style = "Bold";
+          italic.family = "JetBrainsMono Nerd Font";
+          italic.style = "Italic";
+          bold_italic.family = "JetBrainsMono Nerd Font";
+          bold_italic.style = "Bold Italic";
+        };
+
+        colors = {
+          # Default colors;
+          primary = {
+            background = "0x282828";
+            foreground = "0xeeeeee";
+          };
+          # Normal colors;
+          normal = {
+            black = "0x282828";
+            red = "0xf43753";
+            green = "0xc9d05c";
+            yellow = "0xffc24b";
+            blue = "0xb3deef";
+            magenta = "0xd3b987";
+            cyan = "0x73cef4";
+            white = "0xeeeeee";
+          };
+          # Bright colors;
+          bright = {
+            black = "0x4c4c4c";
+            red = "0xf43753";
+            green = "0xc9d05c";
+            yellow = "0xffc24b";
+            blue = "0xb3deef";
+            magenta = "0xd3b987";
+            cyan = "0x73cef4";
+            white = "0xfeffff";
+          };
+        };
+      };
     };
-  };
 }
