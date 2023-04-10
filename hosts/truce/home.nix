35a79c0c3058aa4c4b5e453124e8d659caa0b38e
Quido Meijer <quidome@gmail.com>
2023-04-10 21:07:39 +0200

    Add home config

--- hosts/truce/home.nix
+++ hosts/truce/home.nix
@@ -1,8 +1,33 @@
-{ config, ... }:
+{ config, pkgs, lib, ... }:
 {
+  imports = [
+    ../../home
+    ./vars.nix
+  ];
+
   home = {
     stateVersion = "22.11";
+    packages = with pkgs; [
+      git-crypt
+    ];
   };
 
   programs.home-manager.enable = true;
+  programs = {
+    alacritty.enable = true;
+
+    direnv.enable = true;
+    direnv.nix-direnv.enable = true;
+
+    git.enable = true;
+
+    htop.enable = true;
+    htop.settings.show_program_path = true;
+
+    ssh.enable = true;
+
+    tmux.enable = true;
+
+    zsh.enable = true;
+  };
 }
