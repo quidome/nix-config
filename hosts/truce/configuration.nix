8454233e19643f785b70d0f7362df974bc758447
Quido Meijer <quidome@gmail.com>
2023-04-10 21:36:59 +0200

    Grouping of programs and services

--- hosts/truce/configuration.nix
+++ hosts/truce/configuration.nix
@@ -46,14 +46,17 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-  programs.zsh.enable = true;
+  programs = {
+    gnupg.agent.enable = true;
+    gnupg.agent.enableSSHSupport = true;
 
-  services.openssh.enable = true;
-  services.pipewire.enable = true;
+    zsh.enable = true;
+  };
+
+  services = {
+    openssh.enable = true;
+    pipewire.enable = true;
+  };
 
   system.stateVersion = "22.11"; # Did you read the comment?
 }
