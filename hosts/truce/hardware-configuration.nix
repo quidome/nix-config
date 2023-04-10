21af24ab2429d5593b234706bdc52615ee609057
Quido Meijer <quidome@gmail.com>
2023-04-10 19:31:05 +0200

    Update hardware config

--- hosts/truce/hardware-configuration.nix
+++ hosts/truce/hardware-configuration.nix
@@ -9,9 +9,9 @@
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
@@ -58,12 +58,12 @@
 
   fileSystems."/boot" =
     {
-      device = "/dev/disk/by-uuid/3089-AE97";
+      device = "/dev/disk/by-uuid/6018-3522";
       fsType = "vfat";
     };
 
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/bff03091-6c20-4ccc-8858-bb36a976a0a2"; }];
+    [{ device = "/dev/disk/by-uuid/41088d46-9bc4-4c06-99e8-de67c257a304"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
