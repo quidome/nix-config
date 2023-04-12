166c402a1a0a8fb5273f0581973e8989207fdaa2
Quido Meijer <quidome@gmail.com>
2023-04-12 19:54:41 +0200

    cleanup code

--- home/homebrew.nix
+++ home/homebrew.nix
@@ -1,34 +1,37 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 with lib;
 let
   zshEnabled = config.programs.zsh.enable;
+  isDarwin = pkgs.stdenv.isDarwin;
 in
 {
-  programs.zsh.initExtra = mkIf zshEnabled ''
-    # Homebrew settings
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-    FPATH="$(brew --prefix)/share/zsh/site-functions:''${FPATH}"
-  '';
-
-  home.sessionVariables = {
-    HOMEBREW_BUNDLE_FILE = "${config.home.homeDirectory}/.config/brewfile";
-  };
-
-  home.file.brewfile = {
-    target = ".config/brewfile";
-    text = ''
-      tap "homebrew/cask"
-      tap "homebrew/core"
-
-      brew "nvm"
-      brew "pyenv-virtualenv"
-      brew "pyenv"
-      
-      cask "obsidian"
-      cask "browserosaurus"
-      cask "gimp"
-      cask "raycast"
-      cask "rectangle"
+  config = mkIf isDarwin {
+    programs.zsh.initExtra = mkIf zshEnabled ''
+      # Homebrew settings
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+      FPATH="$(brew --prefix)/share/zsh/site-functions:''${FPATH}"
     '';
+
+    home.sessionVariables = {
+      HOMEBREW_BUNDLE_FILE = "${config.home.homeDirectory}/.config/brewfile";
+    };
+
+    home.file.brewfile = {
+      target = ".config/brewfile";
+      text = ''
+        tap "homebrew/cask"
+        tap "homebrew/core"
+
+        brew "nvm"
+        brew "pyenv-virtualenv"
+        brew "pyenv"
+      
+        cask "obsidian"
+        cask "browserosaurus"
+        cask "gimp"
+        cask "raycast"
+        cask "rectangle"
+      '';
+    };
   };
 }
--- home/programs/default.nix
+++ home/programs/default.nix
@@ -2,10 +2,8 @@
   imports = [
     ./alacritty.nix
     ./git.nix
-    ./homebrew.nix
     ./neovim.nix
     ./starship.nix
-    ./syncthing.nix
     ./tmux.nix
     ./zsh.nix
   ];
