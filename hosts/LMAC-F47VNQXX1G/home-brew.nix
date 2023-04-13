c5a043e390e26cb605fb1080d3778d090674b963
Quido Meijer <quidome@gmail.com>
2023-04-13 13:55:46 +0200

    Remove check for darwin
    including it directly from the host, we know what kind of host this is

--- hosts/LMAC-F47VNQXX1G/home-brew.nix
+++ hosts/LMAC-F47VNQXX1G/home-brew.nix
@@ -2,36 +2,33 @@
 with lib;
 let
   zshEnabled = config.programs.zsh.enable;
-  isDarwin = pkgs.stdenv.isDarwin;
 in
 {
-  config = mkIf isDarwin {
-    programs.zsh.initExtra = mkIf zshEnabled ''
-      # Homebrew settings
-      eval "$(/opt/homebrew/bin/brew shellenv)"
-      FPATH="$(brew --prefix)/share/zsh/site-functions:''${FPATH}"
-    '';
+  programs.zsh.initExtra = mkIf zshEnabled ''
+    # Homebrew settings
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    FPATH="$(brew --prefix)/share/zsh/site-functions:''${FPATH}"
+  '';
 
-    home.sessionVariables = {
-      HOMEBREW_BUNDLE_FILE = "${config.home.homeDirectory}/.config/brewfile";
-    };
+  home.sessionVariables = {
+    HOMEBREW_BUNDLE_FILE = "${config.home.homeDirectory}/.config/brewfile";
+  };
 
-    home.file.brewfile = {
-      target = ".config/brewfile";
-      text = ''
-        tap "homebrew/cask"
-        tap "homebrew/core"
+  home.file.brewfile = {
+    target = ".config/brewfile";
+    text = ''
+      tap "homebrew/cask"
+      tap "homebrew/core"
 
-        brew "nvm"
-        brew "pyenv-virtualenv"
-        brew "pyenv"
+      brew "nvm"
+      brew "pyenv-virtualenv"
+      brew "pyenv"
       
-        cask "obsidian"
-        cask "browserosaurus"
-        cask "gimp"
-        cask "raycast"
-        cask "rectangle"
-      '';
-    };
+      cask "obsidian"
+      cask "browserosaurus"
+      cask "gimp"
+      cask "raycast"
+      cask "rectangle"
+    '';
   };
 }
