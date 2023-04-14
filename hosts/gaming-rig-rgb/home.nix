e687a0697a49dcde661dfbeca7b11eb22930364f
Quido Meijer <quidome@gmail.com>
2023-04-14 19:58:49 +0200

    Update gaming-rig-config

--- hosts/gaming-rig-rgb/home.nix
+++ hosts/gaming-rig-rgb/home.nix
@@ -1,7 +1,18 @@
-{ config, ... }:
+{ config, pkgs, lib, ... }:
 {
+  imports = [
+    ../../home
+    ./home-vars.nix
+  ];
+
+  my.profile = "workstation";
+
   home = {
     stateVersion = "22.11";
+    packages = with pkgs; [
+      # dev tools
+      postgresql
+    ];
   };
 
   programs.home-manager.enable = true;
--- hosts/gaming-rig-rgb/system.nix
+++ hosts/gaming-rig-rgb/system.nix
@@ -1,19 +1,10 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
-
 {
   imports = [
-    # Include the results of the hardware scan.
-    # ../../modules
     ./hardware-configuration.nix
-    # ./vars.nix
     ./zfs-configuration.nix
+    ../../modules
   ];
-
-  # my.gui.flavor = "kde";
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -36,23 +27,21 @@
   users.users.quidome = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
+    shell = pkgs.zsh;
   };
 
-  environment.systemPackages = with pkgs; [
-    vim
-    curl
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
+  programs = {
+    gnupg.agent.enable = true;
+    gnupg.agent.enableSSHSupport = true;
   };
 
-  services.openssh.enable = true;
-  services.pipewire.enable = true;
+  services = {
+    openssh.enable = true;
+    pipewire.enable = true;
 
+    xserver.desktopManager.plasma5.enable = true;
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   system.stateVersion = "22.11"; # Did you read the comment?
 }
