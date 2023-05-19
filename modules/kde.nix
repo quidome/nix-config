717bc2b3ee2f26b7eed2b9743d534f7d8af1df4a
Quido Meijer <quidome@gmail.com>
2023-05-19 17:57:16 +0200

    Remove packages from kde module
    most of them are installed by default, some of them are not needed
    anymore

--- modules/kde.nix
+++ modules/kde.nix
@@ -28,32 +28,18 @@ in
     environment = {
       # add some desktop applications
       systemPackages = with pkgs; [
-        plasma-pa # needed for managing audio
-        # kde apps
         ark
         aspell
         aspellDicts.en
-        bluedevil
-        gwenview
         hunspell
         kate
-        kde-gtk-config
-        kdeplasma-addons
         kgpg
         kompare
         krename
-        libsForQt5.breeze-gtk
-        libsForQt5.dolphin-plugins
         libsForQt5.kcolorchooser
-        libsForQt5.kio-extras
         libsForQt5.kipi-plugins
-        libsForQt5.plasma-browser-integration
-        libsForQt5.plasma-integration
         libsForQt5.qt5.qttools
-        libsForQt5.xdg-desktop-portal-kde
-        okular
         sddm-kcm
-        spectacle
         yakuake
 
         vlc
