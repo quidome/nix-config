deb9d3be982f965e3862ea7b0ca5d5bd997c11b0
Quido Meijer <quidome@gmail.com>
2023-09-25 23:49:59 +0200

    change linux alacritty fontsize

--- home/programs/alacritty.nix
+++ home/programs/alacritty.nix
@@ -2,8 +2,8 @@
 with lib;
 let
   alacrittyEnabled = config.programs.alacritty.enable;
-  fontSize = (if pkgs.stdenv.isDarwin then 14 else 9);
   font = "FiraCode Nerd Font";
+  fontSize = (if pkgs.stdenv.isDarwin then 14 else 11);
 in
 {
   config.programs.alacritty.settings = mkIf alacrittyEnabled {
