85b663a3b9bc7e5c2dcba02ed9db315292fc2190
Quido Meijer <quidome@gmail.com>
2023-11-30 21:27:56 +0100

    Upgrade to jdk 21

--- hosts/coolding/home.nix
+++ hosts/coolding/home.nix
@@ -20,7 +20,7 @@ in
       postgresql
       poetry
       (python3.withPackages my-python-packages)
-      temurin-bin-17
+      temurin-bin-21
       jetbrains.idea-community
       rnix-lsp
       rustup
