b64b05fa3c7a203cbb5c943498ee643825453467
Quido Meijer <quidome@gmail.com>
2023-12-12 20:53:37 +0100

    Configure flathub remote by default

--- modules/workstation.nix
+++ modules/workstation.nix
@@ -2,7 +2,15 @@
 with lib;
 {
   config = mkIf (config.my.profile == "workstation") {
+    # Install and configure flatpak
+    system.activationScripts = {
+      flathub = ''
+        /run/current-system/sw/bin/flatpak remote-add --system --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+      '';
+    };
+
     services.flatpak.enable = true;
+
     services.openssh.enable = true;
     services.pipewire.enable = true;
   };
