1d3d2a648ec8a48c0f48f9a728eafe54492ec4c7
Quido Meijer <quidome@gmail.com>
2024-01-13 12:48:05 +0100

    make hx config work

--- home/programs/helix/languages.nix
+++ home/programs/helix/languages.nix
@@ -18,32 +18,41 @@ lib.mkIf config.programs.helix.enable {
   ];
 
   programs.helix.languages = {
+    language-server = {
+      #   yaml.language-server = {
+      #     command = lib.getExe pkgs.nodePackages.yaml-language-server;
+      #     args = [ "--stdio" ];
+      #     config = {
+      #       yaml.keyOrdering = false;
+      #     };
+      #   };
+      vscode-json-language-server = {
+        command = lib.getExe nodePackages.vscode-json-languageserver;
+        args = [ "--stdio" ];
+        config = { provideFormatter = true; };
+      };
+    };
+
     language = [
       {
         name = "nix";
         auto-format = true;
-        formatter = {
-          command = "nixpkgs-fmt";
-        };
+        formatter.command = lib.getExe nixpkgs-fmt;
       }
-      {
-        name = "javascript";
-        auto-format = true;
-      }
-      {
-        name = "yaml";
-        config = {
-          yaml.keyOrdering = false;
-        };
-      }
+      #  {
+      #    name = "javascript";
+      #   auto-format = true;
+      # }
       {
         name = "json";
         auto-format = true;
-        language-server.command = "vscode-json-languageserver";
-        formatter = {
-          args = [ "--parser" "json" ];
-          command = "prettier";
-        };
+        language-servers = [
+          "vscode-json-language-server"
+        ];
+        # formatter = {
+        #   args = [ "--parser" "json" ];
+        #   command = "prettier";
+        # };
       }
     ];
   };
