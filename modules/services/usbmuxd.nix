2eda111735bbc9c938b734e34b77930411c984c1
Quido Meijer <quidome@gmail.com>
2024-02-05 14:28:22 +0100

    Use alternative usbmuxd package

--- modules/services/usbmuxd.nix
+++ modules/services/usbmuxd.nix
@@ -2,8 +2,9 @@
 {
   config = lib.mkIf config.services.usbmuxd.enable
     {
+      services.usbmuxd.package = pkgs.usbmuxd2;
       environment.systemPackages = with pkgs; [
         libimobiledevice
-      ];
+              ];
     };
 }
