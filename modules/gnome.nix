dcba80632f17768fcb735c413d8986a8dc0e031b
Quido Meijer <quidome@gmail.com>
2024-03-29 23:59:27 +0100

    chore: remove obsolete line
    Setting is true by default, remove it.

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -39,7 +39,6 @@ in
     services.xserver.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
     services.xserver.displayManager.gdm.enable = true;
-    # services.xserver.displayManager.gdm.wayland = true;
 
     services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
   };
