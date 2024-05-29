b8c55676d95f6789912cd4efbe9b4f516efd54ae
Quido Meijer <quidome@gmail.com>
2024-05-29 19:44:00 +0200

    Refactor kde/plasma

--- modules/my.nix
+++ modules/my.nix
@@ -3,10 +3,10 @@ with lib;
 {
   options.my = {
     gui = mkOption {
-      type = with types; enum [ "none" "gnome" "kde" "sway" "pantheon" "plasma6" ];
+      type = with types; enum [ "none" "gnome" "plasma5" "sway" "pantheon" "plasma6" ];
       default = "none";
       description = ''
-        Which gui to use. Gnome or KDE will install the entire desktop environment. Sway will install the bare minumum. 
+        Which gui to use. Gnome or Plasma will install the entire desktop environment. Sway will install the bare minumum. 
         Defaults to `none`, which makes the system headless.
       '';
       example = "sway";
