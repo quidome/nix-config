00906eea13dd7eb495c6898abb4dcb795919c4ee
Quido Meijer <quidome@gmail.com>
2024-06-18 21:38:04 +0200

    Set up darwin GPG and SSH agent

--- home/profiles/workstation-darwin.nix
+++ home/profiles/workstation-darwin.nix
@@ -7,5 +7,18 @@ in
     home.packages = with pkgs; [
       pinentry_mac
     ];
+
+    home.file.".gnupg/gpg-agent.conf".text = ''
+      pinentry-program ${lib.getExe pkgs.pinentry_mac}
+      enable-ssh-support
+    '';
+
+    programs.gpg.enable = true;
+    programs.zsh.initExtra = ''
+      unset SSH_AGENT_PID
+      export GPG_TTY="$(tty)"
+      export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+      gpgconf --launch gpg-agent
+    '';
   };
 }
