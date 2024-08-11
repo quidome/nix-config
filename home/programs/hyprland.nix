1c601d5cdfbcaba01ef07fdafec0692641ad7cb1
Quido Meijer <quidome@gmail.com>
2024-08-11 23:26:11 +0200

    add more hyprland config

--- home/programs/hyprland.nix
+++ home/programs/hyprland.nix
@@ -1,9 +1,36 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let
   hyprlandEnabled = (config.my.gui == "hyprland");
 in
 {
   config = lib.mkIf hyprlandEnabled {
-    services.avizo.enable = true;
+    my = {
+      programs.wofi.enable = true;
+      xdg.enable = true;
+    };
+
+    gtk = {
+      enable = true;
+      font.name = "Noto Sans";
+      theme.name = "Adwaita";
+      gtk3.extraConfig = {
+        gtk-application-prefer-dark-theme = true;
+      };
+    };
+
+    programs = {
+      alacritty.enable = true;
+    };
+
+    services = {
+      avizo.enable = true;
+      mako.enable = true;
+    };
+
+    xdg.systemDirs.data = [
+      "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
+      "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
+    ];
+
   };
 }
--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -13,16 +13,38 @@ in
       };
 
       systemPackages = with pkgs; [
+        # hyprland deps
         avizo
+        hyprpicker
+        hyprcursor
+        hyprlock
+        hypridle
+        hyprpaper
+        waybar
+
+        # Theming
         glib
+        gnome.adwaita-icon-theme
+        gsettings-desktop-schemas
+        gtk-engine-murrine
+        gtk_engines
+        lxappearance
+
+        # qt theming
+        adwaita-qt
+        libsForQt5.breeze-qt5
+        libsForQt5.qt5ct
+
+        kdePackages.polkit-kde-agent-1
+
         (mkIf networkmanagerEnabled networkmanagerapplet)
         (mkIf tailscaleEnabled tailscale-systray)
       ];
     };
 
     programs.gnupg.agent.enableSSHSupport = true;
-    programs.dconf.enable = true;
     programs.hyprland.enable = true;
+    programs.thunar.enable = true;
 
     # for qt theming
     qt.platformTheme = "qt5ct";
