f776dddd71760cc612bdee7e1d21d26d0b599ac5
Quido Meijer <quidome@gmail.com>
2024-09-07 12:50:09 +0200

    Clean up desktop defaults

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -1,19 +1,9 @@
-{ lib, ...}:
 {
   imports = [
-    ./cosmic.nix
     ./gnome.nix
+    ./hyprland.nix
     ./pantheon.nix
     ./plasma.nix
     ./sway.nix
-    ./hyprland.nix
   ];
-
-  # Enable printing and printer discovery
-  services.printing.enable = lib.mkDefault true;
-  services.avahi = {
-    enable = lib.mkDefault true;
-    nssmdns4 = lib.mkDefault true;
-    openFirewall = lib.mkDefault true;
-  };
 }
