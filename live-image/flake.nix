584d1999a3a6f5cbb90f73cf8bc2cb40dd6d16cf
Quido Meijer <quidome@gmail.com>
2024-09-08 17:55:44 +0200

    Add install scripts to live iso

--- live-image/flake.nix
+++ live-image/flake.nix
@@ -16,6 +16,7 @@
           "${nixos}/nixos/modules/installer/cd-dvd/installation-cd-minimal-new-kernel-no-zfs.nix"
           ./iso/base.nix
           ./iso/bcachefs.nix
+          ./scripts
         ];
       };
     };
