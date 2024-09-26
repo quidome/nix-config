b0888837d5370117e0fc8851b89fd33a406fbde1
Quido Meijer <quidome@gmail.com>
2024-09-26 18:22:59 +0200

    Move default apps to common profile

--- home/default.nix
+++ home/default.nix
@@ -1,4 +1,3 @@
-{ lib, pkgs, ... }:
 {
   imports = [
     ./my
@@ -7,60 +6,4 @@
     ./programs
     ./services
   ];
-
-  home.packages = with pkgs;  [
-    # Base
-    bottom
-    curl
-    dogdns
-    fd
-    fzf
-    git-crypt
-    gitui
-    gnupg
-    gopass
-    jless
-    jq
-    ripgrep
-    shellcheck
-    wget
-    yq-go
-
-    # Useful nix related tools
-    cachix # adding/managing alternative binary caches hosted by Cachix
-    comma # run software from without installing it
-    niv # easy dependency management for nix projects
-    nixpkgs-fmt
-  ];
-
-  programs.bat.enable = true;
-  programs.bat.config.theme = "DarkNeon";
-  programs.bat.config.style = "header,snip";
-
-  programs.direnv.enable = true;
-  programs.direnv.nix-direnv.enable = true;
-
-  programs.eza.enable = true;
-  programs.eza.enableZshIntegration = true;
-
-  programs.git.enable = true;
-
-  programs.helix.enable = true;
-
-  programs.home-manager.enable = true;
-
-  programs.htop.enable = true;
-  programs.htop.settings.show_program_path = true;
-
-  programs.ssh.enable = true;
-  programs.ssh.extraConfig = "AddKeysToAgent yes";
-
-  programs.zellij.enable = lib.mkDefault true;
-
-  programs.zoxide.enable = true;
-  programs.zoxide.options = [ "--cmd j" ];
-
-  programs.zsh.enable = true;
-  programs.zsh.enableCompletion = true;
-  programs.zsh.initExtraBeforeCompInit = "fpath+=($HOME/.zsh/completion/)";
 }
--- home/profiles/common.nix
+++ home/profiles/common.nix
@@ -1,10 +1,38 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
   home = {
     packages = with pkgs; [
-      bitwarden-cli
+      # system
+      bottom
+      fd
+      fzf
+      gnupg
+      ripgrep
+
+      # network
+      curl
+      dogdns
+      wget
+
+      # devops
+      git-crypt
+      gitui
       go
+      shellcheck
+      yq-go
+
+      # tools
+      bitwarden-cli
+      gopass
+      jless
+      jq
       rcm
+
+      # Useful nix related tools
+      cachix # adding/managing alternative binary caches hosted by Cachix
+      comma # run software from without installing it
+      niv # easy dependency management for nix projects
+      nixpkgs-fmt
     ];
 
     sessionPath = [
@@ -17,4 +45,35 @@
       DEV_PATH = "${config.home.homeDirectory}/dev";
     };
   };
+
+  programs.bat.enable = true;
+  programs.bat.config.theme = "DarkNeon";
+  programs.bat.config.style = "header,snip";
+
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
+
+  programs.eza.enable = true;
+  programs.eza.enableZshIntegration = true;
+
+  programs.git.enable = true;
+
+  programs.helix.enable = true;
+
+  programs.home-manager.enable = true;
+
+  programs.htop.enable = true;
+  programs.htop.settings.show_program_path = true;
+
+  programs.ssh.enable = true;
+  programs.ssh.extraConfig = "AddKeysToAgent yes";
+
+  programs.zellij.enable = lib.mkDefault true;
+
+  programs.zoxide.enable = true;
+  programs.zoxide.options = [ "--cmd j" ];
+
+  programs.zsh.enable = true;
+  programs.zsh.enableCompletion = true;
+  programs.zsh.initExtraBeforeCompInit = "fpath+=($HOME/.zsh/completion/)";
 }
