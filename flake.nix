d4905245765035357ce2b2074d3283bd64c4f543
Quido Meijer <quidome@gmail.com>
2024-09-26 15:18:34 +0200

    Simplify flake config
    Before this change, this repository was forked to
    https://github.com/quidome/nix-darwin , where all darwin related code
    will remain.
    
    This gave me the opportunity to strip the darwin configuration and
    focus on the nixos code.
    
    For this MR I copied flake.nix from a project on github. It makes the
    setup simpler and easier to add more hosts.

--- flake.nix
+++ flake.nix
@@ -1,10 +1,5 @@
-# I did not invent this myself, this was starting point:
-# https://gist.github.com/jmatsushita/5c50ef14b4b96cb24ae5268dab613050
-#
-# Initially I will just manage my macbook this way and when I'm
-# more comfortable I'll look into migrating the other systems to flakes as well.
 {
-  description = "quidome's nix flake";
+  description = "quidome's linux nix flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
@@ -12,135 +7,57 @@
 
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    darwin.url = "github:lnl7/nix-darwin/master";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, ... }@inputs:
     let
-      inherit (inputs.darwin.lib) darwinSystem;
-      inherit (inputs.nixpkgs.lib) nixosSystem;
-      inherit (inputs.nixpkgs.lib)
-        attrValues optionalAttrs singleton;
+      system = "x86_64-linux";
 
-      # Configuration for `nixpkgs`
-      nixpkgsConfig = {
-        config.allowUnfree = true;
-        config.permittedInsecurePackages = [ "electron-27.3.11" ];
-
-        overlays = attrValues self.overlays ++ singleton (
-          # Sub in x86 version of packages that don't build on Apple Silicon yet
-          final: prev:
-            (optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
-              inherit (final.pkgs-x86)
-                niv# has an aarch64 build but it won't build
-                purescript
-                ;
-            })
-        );
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+          permittedInsecurePackages = [ "electron-27.3.11" ];
+        };
       };
+      args = inputs;
+
+      mkHost = host: inputs.nixpkgs.lib.nixosSystem {
+        inherit pkgs;
+        modules = [
+          { _module.args = args; }
+          ./hosts/${host}/system.nix
+        ];
+      };
+
+      mkHome = user: host: inputs.home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          { _module.args = args; }
+          ./hosts/${host}/home.nix
+          {
+            home = {
+              username = user;
+              homeDirectory = "/home/${user}";
+            };
+          }
+        ];
+      };
+
     in
     {
+      homeConfigurations = {
+        "quidome@nimbus" = mkHome "quidome" "nimbus";
+        "quidome@beast" = mkHome "quidome" "beast";
+        "quidome@coolding" = mkHome "quidome" "coolding";
+        "quidome@truce" = mkHome "quidome" "truce";
+      };
 
       nixosConfigurations = {
-        beast = nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./hosts/beast/system.nix
-
-            inputs.home-manager.nixosModules.home-manager
-            {
-              nixpkgs = nixpkgsConfig;
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.quidome = import ./hosts/beast/home.nix;
-            }
-          ];
-        };
-
-        coolding = nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./hosts/coolding/system.nix
-
-            inputs.home-manager.nixosModules.home-manager
-            {
-              nixpkgs = nixpkgsConfig;
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.quidome = import ./hosts/coolding/home.nix;
-            }
-          ];
-        };
-
-        nimbus = nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            {
-              nixpkgs = nixpkgsConfig;
-
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.quidome = import ./hosts/nimbus/home.nix;
-            }
-            inputs.home-manager.nixosModules.home-manager
-
-            ./hosts/nimbus/system.nix
-          ];
-        };
-
-        truce = nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./hosts/truce/system.nix
-
-            inputs.home-manager.nixosModules.home-manager
-            {
-              nixpkgs = nixpkgsConfig;
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.quidome = import ./hosts/truce/home.nix;
-            }
-          ];
-        };
-      };
-
-      darwinConfigurations = {
-        LMAC-F47VNQXX1G = darwinSystem {
-          system = "aarch64-darwin";
-          modules = [
-            ./hosts/LMAC-F47VNQXX1G/darwin.nix
-
-            inputs.home-manager.darwinModules.home-manager
-            {
-              nixpkgs = nixpkgsConfig;
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.qmeijer = import ./hosts/LMAC-F47VNQXX1G/home.nix;
-            }
-          ];
-        };
-      };
-
-      # Overlays --------------------------------------------------------------- {{{
-      overlays = {
-        unstable-packages = final: _prev: {
-          unstable = import inputs.nixpkgs-unstable {
-            system = final.system;
-            config.allowUnfree = true;
-          };
-        };
-        # Overlays to add various packages into package set
-        # Overlay useful on Macs with Apple Silicon
-        apple-silicon = final: prev:
-          optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
-            # Add access to x86 packages system is running Apple Silicon
-            pkgs-x86 = import inputs.nixpkgs {
-              system = "x86_64-darwin";
-              inherit (nixpkgsConfig) config;
-            };
-          };
+        nimbus = mkHost "nimbus";
+        beast = mkHost "beast";
+        coolding = mkHost "coolding";
+        truce = mkHost "truce";
       };
     };
 }
