c166d4ed8980151cbfba7c2a6a13348d13824261
Quido Meijer <quidome@gmail.com>
2024-12-01 17:16:56 +0100

    Upgrade to 24.11

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "quidome's linux nix flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
