fc5425cdebc5df6cd400f4e018fae8a6f4201115
Quido Meijer <quidome@gmail.com>
2025-04-06 21:45:47 +0200

    Add nix-index cache download function

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -39,6 +39,16 @@ in
           done
           unset i
         fi
+
+        download_nixpkgs_cache_index () {
+          filename="index-$(uname -m | sed 's/^arm64$/aarch64/')-$(uname | tr A-Z a-z)"
+          (
+            mkdir -p ~/.cache/nix-index && cd ~/.cache/nix-index
+            # -N will only download a new version if there is an update.
+            wget -q -N https://github.com/nix-community/nix-index-database/releases/latest/download/$filename
+            ln -f $filename files
+          )
+        }      
       '';
     };
   };
