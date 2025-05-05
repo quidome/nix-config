e938b9144f591eec3777c273fc77c53f549237d0
Quido Meijer <quidome@gmail.com>
2025-05-05 13:29:22 +0200

    Set screenshot location

--- home/xdg/default.nix
+++ home/xdg/default.nix
@@ -1,5 +1,17 @@
+{ config, ... }:
+let cfg = config.xdg;
+in
 {
   imports = [
     ./mimeApps.nix
   ];
+
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+
+    extraConfig = {
+      XDG_SCREENSHOTS_DIR = "${cfg.userDirs.pictures}/Screenshots";
+    };
+  };
 }
