2d0981bdb93a80722b6b5d61fa6669fa25e100ba
Quido Meijer <quidome@gmail.com>
2025-05-11 20:43:04 +0200

    add windows to boot menu

--- hosts/beast/system.nix
+++ hosts/beast/system.nix
@@ -8,7 +8,23 @@
     ../../modules
   ];
 
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    windows = {
+      "windows" =
+        let
+          boot-drive = "FS0";
+        in
+        {
+          title = "Windows";
+          efiDeviceHandle = boot-drive;
+          sortKey = "y_windows";
+        };
+    };
+    edk2-uefi-shell.enable = true;
+    edk2-uefi-shell.sortKey = "z_edk2";
+  };
+
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernel.sysctl = { "vm.swappiness" = 1; };
   boot.kernelParams = [
