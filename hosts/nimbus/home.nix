797c29130dcfc514a2e350a7f8b28195dc3f529f
Quido Meijer <quidome@gmail.com>
2025-05-24 14:08:44 +0200

    Add shikane for output switching

--- hosts/nimbus/home.nix
+++ hosts/nimbus/home.nix
@@ -1,5 +1,4 @@
-{ config, lib, ... }:
-with lib;
+{ config, lib, pkgs, ... }:
 {
   imports = [
     ./shared.nix
@@ -8,18 +7,75 @@ with lib;
   ];
 
   home.stateVersion = "24.05";
+  home.packages = with pkgs; [ shikane ];
 
-  services.kanshi.settings = mkIf (builtins.elem config.settings.gui [ "sway" "hyprland" ]) [
-    {
-      profile.name = "undocked";
-      profile.outputs = [{ criteria = "eDP-1"; status = "enable"; scale = 1.0; }];
-    }
-    {
-      profile.name = "home-office";
-      profile.outputs = [
-        { criteria = "Dell Inc. DELL P3424WE FB6Y6T3"; status = "enable"; scale = 1.0; position = "0,0"; }
-        { criteria = "eDP-1"; status = "disable"; }
-      ];
-    }
+  wayland.windowManager.hyprland.settings.bindl = [
+    "SHIFT, code:133, exec, shikanectl switch disable-external"
   ];
+
+  services.shikane = {
+    enable = true;
+    settings.profile = [
+      {
+        name = "home";
+        output = [
+          {
+            search = [ "m=0x14F9" "s=" "v=Sharp Corporation" ];
+            enable = true;
+            position = { x = 3440; y = 240; };
+            scale = 1.0;
+            transform = "normal";
+          }
+          {
+            search = [ "m=DELL P3424WE" "s=FB6Y6T3" ];
+            enable = true;
+            position = { x = 0; y = 0; };
+            scale = 1.0;
+            transform = "normal";
+          }
+        ];
+      }
+      {
+        name = "default";
+        output = [
+          {
+            match = "eDP-1";
+            enable = true;
+            position = { x = 0; y = 0; };
+            scale = 1.0;
+            transform = "normal";
+          }
+          {
+            match = "DP-3";
+            enable = true;
+            position = { x = 1920; y = 0; };
+            scale = 1.0;
+            transform = "normal";
+          }
+        ];
+      }
+      {
+        name = "standalone";
+        output = [
+          {
+            match = "eDP-1";
+            enable = true;
+            position = {
+              x = 0;
+              y = 0;
+            };
+            scale = 1.0;
+            transform = "normal";
+          }
+        ];
+      }
+      {
+        name = "disable-external";
+        output = [
+          { match = "eDP-1"; enable = true; }
+          { match = "DP-3"; enable = false; }
+        ];
+      }
+    ];
+  };
 }
