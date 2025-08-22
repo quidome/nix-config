8f0c2a2a20efb683e770a11cefbf411e2c9e9e20
Quido Meijer <quidome@gmail.com>
2025-08-22 17:47:13 +0200

    Don't enable wezterm by default

--- home/profiles/workstation.nix
+++ home/profiles/workstation.nix
@@ -13,7 +13,6 @@ in {
     fonts.fontconfig.enable = mkDefault true;
 
     programs.firefox.enable = mkDefault true;
-    programs.wezterm.enable = mkDefault true;
     programs.wofi.enable = mkDefault config.settings.wayland.enable;
     programs.zed-editor.enable = mkDefault true;
 
