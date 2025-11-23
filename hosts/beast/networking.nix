5c3721ab09ac5d673f9531395c92c2664d5d17e0
Quido Meijer <quidome@gmail.com>
2025-11-23 21:07:49 +0100

    improve network setup

--- hosts/beast/networking.nix
+++ hosts/beast/networking.nix
@@ -15,15 +15,18 @@ in {
         "10-enp4s0" = {
           matchConfig.Name = "enp4s0";
           networkConfig.DHCP = "yes";
+          dhcpV4Config.RouteMetric = 1024;
         };
         "11-enp42s0" = {
           matchConfig.Name = "enp42s0";
-          networkConfig.DHCP = "no";
+          networkConfig.DHCP = "yes";
+          dhcpV4Config.RouteMetric = 2048;
           linkConfig.RequiredForOnline = "no";
         };
         "12-enp47s0f3u4u3" = {
           matchConfig.Name = "enp47s0f3u4u3";
-          networkConfig.DHCP = "no";
+          networkConfig.DHCP = "yes";
+          dhcpV4Config.RouteMetric = 3072;
           linkConfig.RequiredForOnline = "no";
         };
       };
