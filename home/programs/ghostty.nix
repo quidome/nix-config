938809a2d0f15e80f319d934dab0f6d87c1860f0
Quido Meijer <quidome@gmail.com>
2025-11-29 10:47:39 +0100

    add more ghostty config

--- home/programs/ghostty.nix
+++ home/programs/ghostty.nix
@@ -1,8 +1,12 @@
-{config, lib, ...}:
 {
-    programs.ghostty.settings = lib.mkIf config.programs.ghostty.enable {
-      font-size = config.settings.terminalFont.size;
-        window-width = 120;
-        window-height = 40;
-    }
+  config,
+  lib,
+  ...
+}: {
+  programs.ghostty.settings = lib.mkIf config.programs.ghostty.enable {
+    font-size = config.settings.terminalFont.size;
+    window-width = 120;
+    window-height = 40;
+    copy-on-select = "clipboard";
+  };
 }
