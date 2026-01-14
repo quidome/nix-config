758400be824a22c1c9fcf9c79ff5a6e97fc7b14a
Quido Meijer <quidome@gmail.com>
2026-01-14 19:34:34 +0100

    Update home.nix

--- hosts/nimbus/home.nix
+++ hosts/nimbus/home.nix
@@ -1,18 +1,4 @@
-{...}: let
-  shikaneOutputDefaults = {
-    enable = true;
-    scale = 1.0;
-    transform = "normal";
-  };
-  shikaneDefaultOutput =
-    {
-      position = {
-        x = 0;
-        y = 0;
-      };
-    }
-    // shikaneOutputDefaults;
-in {
+{
   imports = [
     ./shared.nix
     ./home-vars.nix
@@ -20,63 +6,24 @@ in {
 
   home.stateVersion = "25.11";
 
-  wayland.windowManager.hyprland.settings.bindl = [
-    "SHIFT, code:133, exec, shikanectl switch disable-external"
-  ];
-
-  services.shikane = {
-    settings.profile = [
+  services.kanshi = {
+    enable = true;
+    settings = [
       {
-        name = "home";
-        output = [
+        profile.name = "laptop-only";
+        profile.outputs = [
           {
-            search = ["m=0x14F9" "s=" "v=Sharp Corporation"];
-            enable = false;
-          }
-          ({search = ["m=DELL P3424WE" "s=FB6Y6T3"];} // shikaneDefaultOutput)
-        ];
-      }
-      {
-        name = "default";
-        output = [
-          ({match = "eDP-1";} // shikaneDefaultOutput)
-          ({
-              search = "n/^DP-[1-9]";
-              position = {
-                x = 1920;
-                y = 0;
-              };
-            }
-            // shikaneOutputDefaults)
-        ];
-      }
-      {
-        name = "standalone";
-        output = [({match = "eDP-1";} // shikaneDefaultOutput)];
-      }
-      {
-        name = "disable-internal";
-        output = [
-          {
-            match = "eDP-1";
-            enable = false;
-          }
-          {
-            search = "n/^DP-[1-9]";
-            enable = true;
-          }
-        ];
-      }
-      {
-        name = "disable-external";
-        output = [
-          ({match = "eDP-1";} // shikaneDefaultOutput)
-          {
-            search = "n/^DP-[1-9]";
-            enable = false;
+            criteria = "Sharp Corporation 0x14F9 Unknown";
+            status = "enable";
+            mode = "1920x1200@59.950";
+            scale = 1.15;
           }
         ];
       }
     ];
   };
+
+  wayland.windowManager.hyprland.settings.bindl = [
+    "SHIFT, code:133, exec, shikanectl switch disable-external"
+  ];
 }
