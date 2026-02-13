5d8ab3b83cc88e88c692e369d1df580d458ad7d0
Quido Meijer <quidome@gmail.com>
2026-02-13 19:28:33 +0100

    Set shell-integration-features for ghostty
    To prevent terminal issues from happening when ssh-ing into remote machines

--- home/programs/ghostty.nix
+++ home/programs/ghostty.nix
@@ -12,6 +12,7 @@ in {
     window-width = mkDefault 120;
     window-height = mkDefault 40;
     copy-on-select = mkDefault "clipboard";
+    shell-integration-features = mkDefault "ssh-terminfo";
     theme = mkDefault "light:TokyoNight Day,dark:TokyoNight";
   };
 }
