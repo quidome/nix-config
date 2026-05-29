da37bd59f0d490b8c3f2a0a3f9677fe14ce18846
Quido Meijer <quidome@gmail.com>
2026-05-29 17:50:38 +0200

    chore(flake): migrate inputs to 26.05

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "quidome's linux nix flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    catppuccin.url = "github:catppuccin/nix/release-25.11";
+    catppuccin.url = "github:catppuccin/nix/release-26.05";
 
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     disko.url = "github:nix-community/disko";
