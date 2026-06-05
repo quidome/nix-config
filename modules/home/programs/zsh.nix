6347cae710e295fd114a05f7b2f198cc28a07230
Quido Meijer <quidome@gmail.com>
2026-06-05 17:55:38 +0200

    refactor(zsh): merge program attribute assignments

--- modules/home/programs/zsh.nix
+++ modules/home/programs/zsh.nix
@@ -9,61 +9,63 @@ in {
   config = mkIf cfg.enable {
     home.file.".env.d/70-dev.sh".source = ./zsh/dev.sh;
 
-    # enable starship
-    programs.starship.enable = true;
+    programs = {
+      # enable starship
+      starship.enable = true;
 
-    programs.fzf = {
-      enable = true;
-      enableZshIntegration = true;
-      historyWidgetOptions = [
-        "--height 40%"
-        "--layout=reverse"
-        "--border"
-      ];
-    };
-
-    programs.zsh = {
-      shellAliases = {
-        find = "noglob find";
+      fzf = {
+        enable = true;
+        enableZshIntegration = true;
+        historyWidgetOptions = [
+          "--height 40%"
+          "--layout=reverse"
+          "--border"
+        ];
       };
 
-      enableCompletion = true;
-      autosuggestion.enable = true;
-      history = {
-        size = 50000;
-        save = 50000;
-        ignoreDups = true;
+      zsh = {
+        shellAliases = {
+          find = "noglob find";
+        };
+
+        enableCompletion = true;
+        autosuggestion.enable = true;
+        history = {
+          size = 50000;
+          save = 50000;
+          ignoreDups = true;
+        };
+
+        defaultKeymap = "emacs";
+
+        syntaxHighlighting.enable = true;
+
+        initContent = ''
+          # unfortunally a few system paths end up in front of my profile path
+          # this just adds the path (again) before the other paths
+          export PATH=${config.home.profileDirectory}/bin:$PATH
+
+          # source all .sh file in .env.d
+          if [ -d "$HOME"/.env.d ]; then
+            for i in "$HOME"/.env.d/*.sh; do
+              if [ -r $i ]; then
+                . $i
+              fi
+            done
+            unset i
+          fi
+
+          download_nixpkgs_cache_index () {
+            filename="index-$(uname -m | sed 's/^arm64$/aarch64/')-$(uname | tr A-Z a-z)"
+            (
+              mkdir -p ~/.cache/nix-index && cd ~/.cache/nix-index
+              # -N will only download a new version if there is an update.
+              wget -q -N https://github.com/nix-community/nix-index-database/releases/latest/download/$filename
+              ln -f $filename files
+            )
+          }
+        '';
       };
-
-      defaultKeymap = "emacs";
-
-      syntaxHighlighting.enable = true;
-
-      initContent = ''
-        # unfortunally a few system paths end up in front of my profile path
-        # this just adds the path (again) before the other paths
-        export PATH=${config.home.profileDirectory}/bin:$PATH
-
-        # source all .sh file in .env.d
-        if [ -d "$HOME"/.env.d ]; then
-          for i in "$HOME"/.env.d/*.sh; do
-            if [ -r $i ]; then
-              . $i
-            fi
-          done
-          unset i
-        fi
-
-        download_nixpkgs_cache_index () {
-          filename="index-$(uname -m | sed 's/^arm64$/aarch64/')-$(uname | tr A-Z a-z)"
-          (
-            mkdir -p ~/.cache/nix-index && cd ~/.cache/nix-index
-            # -N will only download a new version if there is an update.
-            wget -q -N https://github.com/nix-community/nix-index-database/releases/latest/download/$filename
-            ln -f $filename files
-          )
-        }
-      '';
     };
   };
 }
