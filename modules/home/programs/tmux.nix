ff209b2a8a4921919fc9f448c44c1523980f4dc9
Quido Meijer <quidome@gmail.com>
2026-06-20 16:57:14 +0200

    Set clock to 24h

--- modules/home/programs/tmux.nix
+++ modules/home/programs/tmux.nix
@@ -5,5 +5,9 @@
 }: {
   config = lib.mkIf config.programs.tmux.enable {
     home.file.".env.d/05-always-tmux.sh".source = ./tmux/always-tmux.sh;
+
+    programs.tmux.extraConfig = ''
+      set -g clock-mode-style 24
+    '';
   };
 }
