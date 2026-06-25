e1223925eae2685f1c1b405030b589d09eeffeac
Quido Meijer <quidome@gmail.com>
2026-06-25 20:46:54 +0200

    update zellij startup

--- modules/home/programs/zellij/always-zellij.sh
+++ modules/home/programs/zellij/always-zellij.sh
@@ -18,9 +18,6 @@ fi
 
 
 if [[ $TERM != "screen-256color" && $TERM != "linux" && -z "$ZELLIJ" ]] ; then
-    if [[ "$(zellij ls -s)" = "default-session" ]]; then
-        zellij attach 'default-session'
-    else
-        zellij attach -c 'default-session'
-    fi
+    session_name="auto-${HOSTNAME:-host}-$$"
+    zellij attach -c "$session_name"
 fi
