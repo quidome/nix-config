78f79d2456a5c18ddd031b75b1932d3de2db346f
Quido Meijer <quidome@gmail.com>
2026-06-25 23:33:46 +0200

    fix(workstation): use gnome libsecret for electron messengers

--- modules/system/profiles/workstation.nix
+++ modules/system/profiles/workstation.nix
@@ -6,6 +6,39 @@
 }:
 with lib; let
   isWorkstation = config.settings.gui != "none";
+  electronSecretStoreFlags = "--password-store=gnome-libsecret";
+
+  elementDesktop = pkgs.symlinkJoin {
+    name = "element-desktop";
+    paths = [pkgs.element-desktop];
+    nativeBuildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      rm $out/bin/element-desktop
+      makeWrapper ${lib.getExe pkgs.element-desktop} $out/bin/element-desktop \
+        --add-flags "${electronSecretStoreFlags}"
+
+      rm $out/share/applications/element-desktop.desktop
+      cp ${pkgs.element-desktop}/share/applications/element-desktop.desktop $out/share/applications/element-desktop.desktop
+      substituteInPlace $out/share/applications/element-desktop.desktop \
+        --replace-fail "Exec=element-desktop %u" "Exec=element-desktop ${electronSecretStoreFlags} %u"
+    '';
+  };
+
+  signalDesktop = pkgs.symlinkJoin {
+    name = "signal-desktop";
+    paths = [pkgs.signal-desktop];
+    nativeBuildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      rm $out/bin/signal-desktop
+      makeWrapper ${lib.getExe pkgs.signal-desktop} $out/bin/signal-desktop \
+        --add-flags "${electronSecretStoreFlags}"
+
+      rm $out/share/applications/signal.desktop
+      cp ${pkgs.signal-desktop}/share/applications/signal.desktop $out/share/applications/signal.desktop
+      substituteInPlace $out/share/applications/signal.desktop \
+        --replace-fail "Exec=signal-desktop %U" "Exec=signal-desktop ${electronSecretStoreFlags} %U"
+    '';
+  };
 in {
   config = mkIf isWorkstation {
     hardware.bluetooth.input.General.UserspaceHID = mkDefault true;
@@ -15,9 +48,9 @@ in {
       cameractrls-gtk3
       firefox
       mani
-      element-desktop
+      elementDesktop
       obsidian
-      signal-desktop
+      signalDesktop
       spotify
       pandoc
       pavucontrol
