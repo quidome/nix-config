aea80bea28c846b9183777351dc61e7578a270b4
Quido Meijer <quidome@gmail.com>
2026-06-27 21:07:18 +0200

    Simplify monitor config on bea

--- hosts/bea/home.nix
+++ hosts/bea/home.nix
@@ -12,83 +12,6 @@
 
   settings.terminalFont.size = 10;
 
-  services.shikane = lib.mkIf (config.settings.gui == "hyprland") {
-    enable = true;
-    settings.profile = [
-      {
-        name = "desktop";
-        output = [
-          {
-            match = "DP-1";
-            enable = true;
-            position = {
-              x = 0;
-              y = 0;
-            };
-            scale = 1.0;
-            transform = "normal";
-          }
-          {
-            match = "Samsung Electric Company LC32G5xT HK2W200965";
-            enable = false;
-          }
-        ];
-      }
-      {
-        name = "gaming";
-        output = [
-          {
-            match = "DP-1";
-            enable = false;
-          }
-          {
-            match = "Samsung Electric Company LC32G5xT HK2W200965";
-            enable = true;
-            position = {
-              x = 0;
-              y = 0;
-            };
-            scale = 1.0;
-            transform = "normal";
-          }
-        ];
-      }
-      {
-        name = "dual";
-        output = [
-          {
-            match = "Samsung Electric Company LC32G5xT HK2W200965";
-            enable = true;
-            position = {
-              x = 0;
-              y = 0;
-            };
-            scale = 1.0;
-            transform = "normal";
-          }
-          {
-            match = "DP-1";
-            enable = true;
-            position = {
-              x = 2560;
-              y = 0;
-            };
-            scale = 1.0;
-            transform = "normal";
-          }
-        ];
-      }
-    ];
-  };
-
-  systemd.user.services.shikane = lib.mkIf (config.settings.gui == "hyprland") {
-    Unit = {
-      After = lib.mkForce ["hyprland-session.target"];
-      PartOf = lib.mkForce ["hyprland-session.target"];
-    };
-    Install.WantedBy = lib.mkForce ["hyprland-session.target"];
-  };
-
   wayland.windowManager.hyprland.settings = lib.mkIf (config.settings.gui == "hyprland") {
     device = [
       {
@@ -101,6 +24,11 @@
         left_handed = false;
         natural_scroll = false;
       }
+    ];
+
+    config.monitor = [
+      "desc:Dell Inc. DELL P3424WE FB6Y6T3, preferred, auto, 1"
+      "desc:Samsung Electric Company LC32G5xT HK2W200965, disable"
     ];
   };
 }
