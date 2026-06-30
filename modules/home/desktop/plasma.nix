704a1bfe4d9519691a1109d6e9d0eecf83d2270d
Quido Meijer <quidome@gmail.com>
2026-06-30 23:22:37 +0200

    feat(plasma): extend konsole profile defaults

--- modules/home/desktop/plasma.nix
+++ modules/home/desktop/plasma.nix
@@ -22,18 +22,23 @@ in {
       NIXOS_OZONE_WL = "1";
     };
 
-    settings.terminal = mkDefault "konsole";
-
     xdg.dataFile = mkIf useKonsoleTheme {
       "konsole/Catppuccin_Latte.colorscheme".source = ../dotfiles/Catppuccin_Latte.colorscheme;
       "konsole/Catppuccin_Mocha.colorscheme".source = ../dotfiles/Catppuccin_Mocha.colorscheme;
       "konsole/${konsoleTheme}.profile".text = ''
         [Appearance]
         ColorScheme=${konsoleTheme}
+        Font=${config.settings.terminalFont.name},${toString config.settings.terminalFont.size},-1,5,400,0,0,0,0,0,0,0,0,0,0,1,,0,0
 
         [General]
         Name=${konsoleThemeLabel}
         Parent=FALLBACK/
+        TerminalColumns=120
+        TerminalRows=40
+
+        [Scrolling]
+        HistorySize=10000
+        ScrollBarPosition=2
       '';
     };
 
