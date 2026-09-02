bafceae7e50a0bb52f3209ca38f5eae580085af8
Quido Meijer <quidome@gmail.com>
2026-09-02 20:00:51 +0200

    feat(system): install Herdr on all hosts

--- modules/system/profiles/common.nix
+++ modules/system/profiles/common.nix
@@ -4,7 +4,32 @@
   pkgsUnstable,
   ...
 }:
-with lib; {
+with lib; let
+  herdr = pkgs.stdenvNoCC.mkDerivation {
+    pname = "herdr";
+    version = "0.8.2";
+
+    src = pkgs.fetchurl {
+      url = "https://github.com/herdrdev/herdr/releases/download/v0.8.2/herdr-linux-x86_64";
+      hash = "sha256-l2FQoU1JDJSyQ+ouGn6y37Z/EuNrGC25CTb2co5q7PQ=";
+    };
+
+    dontUnpack = true;
+    dontBuild = true;
+
+    installPhase = ''
+      install -Dm755 "$src" "$out/bin/herdr"
+    '';
+
+    meta = {
+      description = "Terminal workspace manager for AI coding agents";
+      homepage = "https://herdr.dev";
+      license = lib.licenses.asl20;
+      mainProgram = "herdr";
+      platforms = ["x86_64-linux"];
+    };
+  };
+in {
   boot = {
     loader.systemd-boot.enable = mkDefault true;
     loader.efi.canTouchEfiVariables = mkDefault true;
@@ -63,6 +88,7 @@ with lib; {
     # devops tooling
     cilium-cli
     helmfile
+    herdr
     ipcalc
     k9s
     kubectl
