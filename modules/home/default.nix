86e05f5fd5a937d3b48acdf16881150e1f1c637e
Quido Meijer <quidome@gmail.com>
2026-09-03 20:23:39 +0200

    chore(home): remove unused theme and terminal configuration

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -4,6 +4,5 @@
     ./profiles
     ./programs
     ./settings.nix
-    ./theme
   ];
 }
--- modules/home/programs/default.nix
+++ modules/home/programs/default.nix
@@ -4,7 +4,6 @@
     ./git.nix
     ./gpg.nix
     ./helix
-    ./wezterm.nix
     ./neovim
     ./starship.nix
     ./zed-editor.nix
